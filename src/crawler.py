3c79647f9eca13cc729f2411329a97fb12006fdb
dsuther12 <dsuther@bgsu.edu>
2025-06-22 18:05:09 -0400

    Extract links from dropdown menu on main wiki page

--- src/crawler.py
+++ src/crawler.py
@@ -5,17 +5,42 @@ import json
 from bs4 import BeautifulSoup
 import re
 
+# Gets the page links in dropdown menus on the main page. E.g. Armor, Shields, Characters, etc.
+def get_page_links():
+    main_url = "https://eldenring.wiki.fextralife.com"
+    response = requests.get(main_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.select('a')
+    pattern = r'^/'
+    page_links = set()
+    for link in links:
+        if 'href' in link.attrs and 'target' in link.attrs and re.match(pattern, link['href']):
+            page_links.add(main_url + link['href'])
 
-response = requests.get("https://eldenring.wiki.fextralife.com/Elden+Ring+Wiki")
-# print(response.text)
-soup = BeautifulSoup(response.text, 'html.parser')
-links = soup.select('a')
+    return page_links
 
-pattern = r'^/'
-for link in links:
-    if 'href' in link.attrs and 'target' in link.attrs:
-        if re.match(pattern, link['href']):
-            print(link['href'])
+# Gets the HTML of the pages within the dropdown menu links on the main wiki page
+def get_page_links_html():
+    page_links = get_page_links()
+    count = 0
+    page_html_dict = {}
+    for link in page_links:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        match = re.search(r'/[^/]+$', link)
+        dict_keys = match.group().replace('/', '').replace('+', '_').lower()
+        page_html_dict[dict_keys] = {'link': link, 'html': soup}
+
+    return page_html_dict
+
+
+pages_html_dict = get_page_links_html()
+
+
+
+
+
+
 
 
 
